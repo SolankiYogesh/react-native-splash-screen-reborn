ebfb888af2769f1abe615290a56bcab67da1ee7b
Solanki Yogesh <solankiyogesh3500@gmail.com>
2024-11-14 16:09:29 +0530

    chore: code clean up

--- ios/SplashScreen.mm
+++ ios/SplashScreen.mm
@@ -3,17 +3,6 @@
 @implementation SplashScreen
 RCT_EXPORT_MODULE()
 
-// // Example method
-// // See // https://reactnative.dev/docs/native-modules-ios
-// RCT_EXPORT_METHOD(multiply:(double)a
-//                   b:(double)b
-//                   resolve:(RCTPromiseResolveBlock)resolve
-//                   reject:(RCTPromiseRejectBlock)reject)
-// {
-//     NSNumber *result = @(a * b);
-
-//     resolve(result);
-// }
 static bool waiting = true;
 static bool addedJsLoadErrorObserver = false;
 static UIView* loadingView = nil;
